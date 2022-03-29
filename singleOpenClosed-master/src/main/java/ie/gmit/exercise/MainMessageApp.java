cb64dd8fcd510700d157495a3fe6cfe8c603b746
SIMON GABORIT - STUDENT <G00377155@gmit.ie>
2022-03-29 10:30:44 +0100

    First commit

---
+++ singleOpenClosed-master/src/main/java/ie/gmit/exercise/MainMessageApp.java
@@ -0,0 +1,19 @@
+package ie.gmit.exercise;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MainMessageApp {
+
+	public static void main(String[] args) throws IOException {
+
+		Formatter formatter = new JSONFormatter();//creates formatter
+
+		PrintWriter writer = new PrintWriter(System.out);
+
+		Message msg = new Message("This is a message");
+		MessagePrinter printer = new MessagePrinter();
+		printer.writeMessage(msg, writer, formatter);
+	}
+}
